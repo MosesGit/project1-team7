710eb931ce332b907acf8801ddfee6d05c90abc3
mtam02 <mtam2@u.rochester.edu>
2016-09-29 22:58:46 -0400

    Pokemon Generation Wiki

---
+++ colophon.php
@@ -0,0 +1,33 @@
+<?php $parentTitle = "Pokemon Generation Wiki"; $currentTitle = "Colophon"; $separator = " - "; include "inc/top.inc";?>
+
+		<?php include "inc/nav.inc";?>
+
+		<article class="col-md-8">
+			
+			<h2>Article heading</h2>
+			
+			<img src="xxx.jpg" alt="xxx">
+			
+			<p>Article paragraph</p>
+
+		</article>
+
+		<aside class="col-md-4">
+
+			<h2>Aside heading</h2>
+			
+			<img src="xxx.png" alt="xxx">
+
+			<p>Aside paragraph</p>
+
+		</aside>
+
+		<?php include "inc/footer.inc";?>
+
+	</div><!--.container-->
+
+	<?php include "inc/scripts.inc";?>
+	
+	</body>
+
+</html>
--- index.php
+++ index.php
@@ -6,20 +6,22 @@
 		
 			<h2>Overview</h2>
 
-			<img class="img-responsive" src="http://toucharcade.com/wp-content/uploads/2016/07/landscape-1456483171-pokemon2.jpg" alt="overview">
+			<img class="img-responsive" src="images/landscape-1456483171-pokemon2.jpg" alt="overview">
 
-			<p>All of the licensed Pokémon properties overseen by The Pokémon Company are divided roughly by generation. These generations are roughly chronological divisions by release; when an official sequel in the main role-playing game series is released that features new Pokémon, characters, and gameplay concepts, that sequel is considered the start of a new generation of the franchise. The main games and their spin-offs, the anime, manga and trading card game are all updated with the new Pokémon properties each time a new generation begins. The franchise began its sixth generation with Pokémon X and Y, which were released worldwide on October 12, 2013.
-</p>
+			<p>All of the licensed Pokémon properties overseen by The Pokémon Company are divided roughly by generation. These generations are roughly chronological divisions by release; when an official sequel in the main role-playing game series is released that features new Pokémon, characters, and gameplay concepts, that sequel is considered the start of a new generation of the franchise. The main games and their spin-offs, the anime, manga and trading card game are all updated with the new Pokémon properties each time a new generation begins. The franchise began its sixth generation with Pokémon X and Y, which were released worldwide on October 12, 2013.</p>
 
 		</article>
 
 		<aside class="col-md-4">
 
 			<h2>What is Pokemon? </h2>
-			<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f7/English_Pok%C3%A9mon_logo.svg/269px-English_Pok%C3%A9mon_logo.svg.png" alt="logo">
+
+			<img class="img-responsive" src="images/English_Pokémon_logo.svg.png" alt="logo">
 
 			<p>Pokémon is a series of video games developed by Game Freak and Creatures Inc. and published by Nintendo as part of the Pokémon media franchise. First released in 1996 in Japan for the Game Boy, the main series of role-playing video games (RPGs) has continued on each generation of Nintendo's handhelds.</p>
-			<p>Games are commonly released in pairs—each with slight variations—and then an enhanced remake of the games is released a few years after the original releases. While the main series consists of role-playing games, spinoffs encompass other genres, such as action role-playing, puzzle, and digital pet games. As of February 2016, more than 279 million units have been sold worldwide, more than 200 million of which from the main series,[1] making it the second best-selling video game franchise, behind only Nintendo's own Mario franchise. The franchise's mascot is Pikachu.</p>
+
+			<p>Games are commonly released in pairs—each with slight variations—and then an enhanced remake of the games is released a few years after the original releases. While the main series consists of role-playing games, spinoffs encompass other genres, such as action role-playing, puzzle, and digital pet games. As of February 2016, more than 279 million units have been sold worldwide, more than 200 million of which from the main series, making it the second best-selling video game franchise, behind only Nintendo's own Mario franchise. The franchise's mascot is Pikachu.</p>
+
 		</aside>
 
 		<?php include "inc/footer.inc";?>
