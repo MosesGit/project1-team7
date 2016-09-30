9d93fba3185a4ac529e09cb38cd6f7c3d7d12a1f
ylu21 <ylu21@u.rochester.edu>
2016-09-29 21:55:57 -0400

    pokemon content added

--- child2.php
+++ child2.php
@@ -1,24 +1,21 @@
-<?php $parentTitle = "ParentTitle"; $currentTitle = "CurrentTitle 2"; $separator = " - "; include "inc/top.inc";?>
+<?php $parentTitle = "Pokemon Generation Wiki"; $currentTitle = "3th-4th Gen"; $separator = " - "; include "inc/top.inc";?>
 
 		<?php include "inc/nav.inc";?>
 
 		<article class="col-md-8">
 			
-			<h2>Article Heading</h2>
+			<h2>3rd - 4th Generations</h2>
 			
-			<img src="images/xxx.jpg" alt="xxx">
+			<img src="http://www.spriters-resource.com/resources/sheets/62/64887.png" alt="3rdGen">
 			
-			<p>Article paragraph</p>
-	
+			<p>Pokémon entered its third generation with the 2002 release of Pokémon Ruby and Sapphire for Game Boy Advance and continued with the Game Boy Advance remakes of Pokémon Red and Green, Pokémon FireRed and LeafGreen (Red and Green representing the original Japanese first generation games; territories outside Japan instead saw releases of Red and Blue). An enhanced remake of Pokémon Ruby and Sapphire titled Pokémon Emerald followed after.</p>
+			<p>The third generation introduced 135 new Pokémon (starting with Treecko and ending with Deoxys) for a total of 386 species. It also features a more visually detailed environment compared to previous games, "natures" which affect Pokémon stats, a new 2-on-2 Pokémon battling mechanic, a special ability system applying to each Pokémon in battle, the Pokémon Contest sub-game, the new region of Hoenn, and the ability to select the protagonist's gender. However, this generation also garnered some criticism for leaving out several gameplay features, including the day-and-night system introduced in the previous generation (which was removed due to internal-battery save problems), and it was also the first installment that encouraged the player to collect merely a selected assortment of the total number of Pokémon rather than every existing species (202 out of 386 species are catchable in the Ruby and Sapphire versions).
+</p>
+			<img src="http://1.bp.blogspot.com/-bwkJ_GN8yTY/UaUTitNdAgI/AAAAAAAAL0I/49OupiDWV5o/s1600/Pokemon_HG_SS_sinnoh_Pokemon_by_SanaGX.jpg" alt="4thGen">
+			<p>In 2006, Japan began the fourth generation of the franchise with the release of Pokémon Diamond and Pearl for Nintendo DS. The games were subsequently released in North America on April 22, 2007 and in Australia on June 21, 2007. The game was then later released in the UK and Europe on July 27, 2007.[6] Other main series games in the fourth generation includePokémon Platinum, a director's cut version of Diamond and Pearl in the same vein as Pokémon Yellow, Crystal, and Emerald.[7][8] It was released for the Nintendo DS in Japan on September 13, 2008,[9] in North America on March 22, 2009,[10][11] and in Australia and Europe on May 14, 2009[12] and May 22, 2009[13] respectively. It was also announced that the Generation II games Pokémon Gold and Silver would be remade for the Nintendo DS as Pokémon HeartGold and SoulSilver.[14] First released in Japan on September 12, 2009, the games were later released to North America, Australia, and Europe during March 2010.
+</p>
+			<p>The fourth generation introduces another 107 new species of Pokémon (starting with Turtwig and ending with Arceus), bringing the number of Pokémon species to 493. This generation is the first to have 3D graphics in a main series game, although it is still a mixture of both 3D graphics and sprites. New gameplay concepts include a restructured move-classification system, online multiplayer trading and battling via Nintendo Wi-Fi Connection, the return (and expansion) of the second generation's day-and-night system, the expansion of the third generation's Pokémon Contests into "Super Contests", and the new region of Sinnoh, which has an underground component for multiplayer gameplay in addition to the main overworld. HeartGold and SoulSilver also introduced the Pokeathelon to the Johto region, which consists of many Pokémon based sporting events making use of the stylus.</p>
 		</article>
-
-		<aside class="col-md-4">
-		
-			<h2>Aside Heading</h2>
-			
-			<p>Aside paragraph</p>
-		
-		</aside>
 
 		<?php include "inc/footer.inc";?>
 
--- child3.php
+++ child3.php
@@ -1,24 +1,27 @@
-<?php $parentTitle = "ParentTitle"; $currentTitle = "CurrentTitle 3"; $separator = " - "; include "inc/top.inc";?>
+<?php $parentTitle = "Pokemon Generation Wiki"; $currentTitle = "5 - 7 Generation"; $separator = " - "; include "inc/top.inc";?>
 
 		<?php include "inc/nav.inc";?>
 
 		<article class="col-md-8">
 			
-			<h2>Article Heading</h2>
+			<h2>5th - 7th Generations</h2>
 			
-			<img src="images/xxx.jpg" alt="xxx">
+			<img src="https://c1.staticflickr.com/5/4092/5002685563_ed4bac757c.jpg" alt="5thGen">
 			
-			<p>Article paragraph</p>
-	
+			<p>The fifth generation of Pokémon began on September 18, 2010 with the release of Pokémon Black and White in Japan. They were then released in North America, Europe, and Australia in March 2011. They were released on the Nintendo DS, the same console as its predecessing generation. The games take place in the Unova region. New features include the C-Gear, a feature where players can use Wi-Fi options and customizations; two new battle methods ("Triple Battles", where three Pokémon are sent out at once, and Rotation Battles, where three Pokémon are also sent out at the same time, but the trainer can switch one Pokémon out of the three that are present); "Battle Tests", where trainers battle each other to see who has stronger Pokémon; the Pokémon Musicals (similar to Pokémon Contests), which have trainers use their Pokémon to dance in a theater with other Pokémon; and the ability to never waste Technical Machines (TMs), even when found the first time.
+</p>
+			<p>This generation introduced a total of 156 new Pokémon (beginning with Victini and ending with Genesect), the most of any generation. It was also the first generation where the first new Pokémon in National Pokédex order (Victini) is not a starter. It also introduced another new feature, the seasons, from which two Pokémon (Deerling and Sawsbuck) represent them. Unlike previous generations, which would introduce some species of Pokémon that were evolutionary relatives of older-generation Pokémon, the fifth generation's selection was all-original, in an attempt to make the primary versions feel like a brand-new game.</p>
+			
+			<img src="http://i.imgur.com/Znt5Fee.png" alt="6thGen">
+			
+			<p>On December 24, 2012, Japanese magazine Nintendo Dream posted a greetings card sent out by Game Freak. In the card, Junichi Masuda exclaimed that during 2013, they intend to further evolve the world of Pokémon. On December 29, 2012, a new Pokémon Black 2 and White 2 commercial aired in Japan, and ending with a message, informing Pokémon fans that the latest news would be announced on January 8, 2013. On January 4, 2013, both the Japanese and English official Pokémon website confirmed that an announcement would be made on January 8. On January 7, 2013, the official Japanese website explained that the Nintendo president, Satoru Iwata would hold a 10-minute "Pokémon Direct" video conference to announce the big Pokémon news. On January 8, 2013, Satoru Iwata announced the sixth generation of Pokémon, with the new paired games, Pokémon X and Y, which were released on the Nintendo 3DS on October 12, 2013 worldwide.[2] The X and Y games are rendered in full 3D;[19] however, only select parts of the game can be displayed in stereoscopic 3D.[20] The video introduced the player characters, the starter Pokémon; Grass-type Chespin (Japanese: Harimaron (ハリマロン?)), the Fire-type Fennekin (Japanese: Fokko (フォッコ?)), and the Water-type Froakie (Japanese: Keromatsu (ケロマツ?)), and two other Pokémon, not named until later; a bird-like Pokémon called Yveltal (イベルタル Iberutaru?) having a shape similar to the letter Y and a deer-like Pokémon called Xerneas (ゼルネアス Zeruneasu?) with X-shapes in its eyes. A month later, Sylveon(Japanese: Nymphia (ニンフィア Ninfia?)), a new evolved form of Eevee belonging to the games' new Fairy Type was revealed.[21][22]
+</p>
+			<p>This generation introduced a total of 72 new Pokémon, the new Fairy type, Mega Evolution, the Kalos region, Trainer customization, Super Training, and three new battle modes: Sky Battles, Horde Encounters, and Inverse Battle. This generation is also the first to be compatible with Pokémon Bank.
+</p>		
+			<img src="http://img06.deviantart.net/fa81/i/2016/131/5/6/pokemon_sun_and_moon_starters_by_lani_senpai-da24v2o.jpg" alt="7thGen">
+			<p>During a Nintendo Direct presentation on February 26, 2016, two new Pokémon titles were announced: Pokémon Sun and Moon. The first installments in the seventh generation of Pokémon games, they are set to be released on 18 November 2016.[25] The games will be compatible with other Nintendo 3DS titles, including Pokémon X and Y; Pokémon Omega Ruby and Alpha Sapphire; and the Virtual Console re-releases of Pokemon Red, Blue, and Yellow.
+In July 2016, Niantic & Nintendo released a free-to-play augmented-reality game titled Pokémon Go which was released on Android & iOS devices. Upon its release, the game has become a global phenomenon. Unfortunately, the game has also received a lot of negative criticism.</p>
 		</article>
-
-		<aside class="col-md-4">
-		
-			<h2>Aside Heading</h2>
-			
-			<p>Aside paragraph</p>
-		
-		</aside>
 
 		<?php include "inc/footer.inc";?>
 
