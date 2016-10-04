8f5795d2b3751e01db51058371edb70f5ec47be0
xli62 <xli62@u.rochester.edu>
2016-10-04 18:37:18 -0400

    edit

--- index.php
+++ index.php
@@ -6,12 +6,12 @@
 		
 			<h2>Overview</h2>
 
-			<img class="img-responsive" src="images/landscape-1456483171-Pokemon2.jpg" alt="overview">
+			<img class="img-responsive" src="images/landscape-1456483171-pokemon2.jpg" alt="overview">
 
 			<p>All of the licensed Pokémon properties overseen by The Pokémon Company are divided roughly by generation. These generations are roughly chronological divisions by release; when an official sequel in the main role-playing game series is released that features new Pokémon, characters, and gameplay concepts, that sequel is considered the start of a new generation of the franchise. The main games and their spin-offs, the anime, manga and trading card game are all updated with the new Pokémon properties each time a new generation begins. The franchise began its sixth generation with Pokémon X and Y, which were released worldwide on October 12, 2013.</p>
 
 		</article>
-
+		
 		<aside class="col-md-4">
 
 			<h2>What is Pokémon? </h2>
