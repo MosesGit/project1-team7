a5b6a0e24ab89f5436cc0f8ddce2f5c96c84857e
Yichen Lu <yichen_0@hotmail.com>
2016-10-06 15:15:07 -0400

    Update colophon.php

--- colophon.php
+++ colophon.php
@@ -2,15 +2,36 @@
 
 		<?php include "inc/nav.inc";?>
 
-		<article class="col-md-8">
-			
-			<h2>Article heading</h2>
-			
-			<img src="xxx.jpg" alt="xxx">
-			
-			<p>Article paragraph</p>
-
-		</article>
+		<div class="col-md-8">
+			<div class="row">
+				<div class="parallax-here parallax1">
+					<h2>What we do</h2>
+				</div>
+				<article>
+					<p>We are CSC174 students who wants to create a reference page for pokemon lovers.</p>
+				</article>
+				<div class="parallax-here parallax2">
+					<h2>Who are we</h2>
+				</div>
+				<article>
+					<p class="profile-text">
+						<span class="profile-name"> Moses Chen </span> - Architect
+					</p>
+					<p class="profile-text">
+						<span class="profile-name"> Ming Hang Tam </span> - Framework Wrangler
+					</p>
+					<p class="profile-text">
+						<span class="profile-name"> Yichen Lu </span> - Content Master and interaction Designer
+					</p>
+					<p class="profile-text">
+						<span class="profile-name"> Xingyi Li </span> - CSS Artist
+					</p>
+					<p class="profile-text">
+						<span class="profile-name"> Jimmi Chen </span> - Javascript Engineer
+					</p>
+				</article>
+			</div>
+		</div>
 
 		<aside class="col-md-4">
 
